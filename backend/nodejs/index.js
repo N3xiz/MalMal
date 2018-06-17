d3c06462865b48d395034c595d64d9e9f8be5a27
PhilippFr <philipp.fruh@web.de>
2018-06-17 18:37:52 +0200

    Removes static resources (jquery, bootstrap), replaced with CDN. Highscore sorting in backend. Fixed highscore table. Implemented Word submitting.

--- backend/nodejs/index.js
+++ backend/nodejs/index.js
@@ -23,9 +23,29 @@ app.use(function(err, req, res, next) {
     }
 });
 
+function sortProperties(obj)
+{
+    // convert object into array
+    var sortable=[];
+    for(var key in obj)
+        if(obj.hasOwnProperty(key))
+            sortable.push([key, obj[key]]); // each item is an array in format [key, value]
+
+    // sort items by value
+    sortable.sort(function(b, a)
+    {
+        return a[1]-b[1]; // compare numbers
+    });
+
+    var jsonhighscore = { "highscore" : sortable};
+    return jsonhighscore; // array in format [ [ key1, val1 ], [ key2, val2 ], ... ]
+}
+
+
 app.get('/highscore', (req, res) => {
-    res.status(200).json(highscores);
+    res.status(200).json(sortProperties(highscores));
 });
+
 
 app.post('/highscore', (req, res) => {
     const newScore = req.body;
@@ -33,13 +53,16 @@ app.post('/highscore', (req, res) => {
     for (let user in newScore) {
         if (Number.isInteger(newScore[user])) {
             if (!(highscores[user] > newScore[user])) highscores[user] = newScore[user];
+            //Sort
         }
 
         else {
             res.status(400).json({message: 'The score for ' + user + ' is not an Integer'});
             return;
         }
+
     }
+
 
     res.status(200).json({message: 'Data has been successfully added'});
 });
